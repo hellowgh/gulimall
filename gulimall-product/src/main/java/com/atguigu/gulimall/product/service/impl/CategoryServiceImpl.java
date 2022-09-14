b9f7c4a5a7e8e286429b080d3e283a8f77108f58
思来享趣 <hellowgh@qq.com>
2022-09-14 22:53:02 +0800

    完成商品服务的3级菜单接口

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +31,34 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+    //    1、查询所有数据
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+
+
+        //    2、组装树形数据
+        //    2.1 找出所有1级菜单
+        List<CategoryEntity> levelMenu1 = categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map(menu -> {
+                    menu.setChildren(getChildren(menu, categoryEntities));
+                    return menu;
+                })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+
+        return levelMenu1;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity node, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(item -> item.getParentCid() == node.getCatId())
+                .map(item -> {
+                    item.setChildren(getChildren(item, all));
+                    return item;
+                })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+
+        return children;
+    }
 }
